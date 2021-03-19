79de56e61954c60717f8ee7d584ff62fe2e115da
Alexandru Ica <alexandruica703@gmail.com>
2021-03-19 17:56:03 +0200

    First tests for `Author`
    Make sure IDs are generated uniquely and the `getID` method is
    thread-safe

---
+++ src/test/java/Bookster/AuthorTest.java
@@ -0,0 +1,61 @@
+package Bookster;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Vector;
+
+public class AuthorTest {
+    private static final ArrayList<String> firstNames = new ArrayList<>();
+    private static final ArrayList<String> lastNames = new ArrayList<>();
+    private static final ArrayList<Author> authors = new ArrayList<>();
+
+    @BeforeAll
+    static void setup() {
+        firstNames.add("Juan");
+        firstNames.add("Pablo");
+        firstNames.add("Jerry");
+        firstNames.add("John");
+        firstNames.add("Tom");
+        firstNames.add("Mariah");
+        firstNames.add("Paul");
+
+        lastNames.ensureCapacity(firstNames.size());
+
+        lastNames.add("Lovelace");
+        lastNames.add("Herbert");
+        lastNames.add("Muriel");
+        lastNames.add("Guy");
+        lastNames.add("Davidson");
+        lastNames.add("Winters");
+        lastNames.add("Sean");
+
+        var random = new Random();
+
+        for (final var name : firstNames) {
+            authors.add(new Author(
+                    name,
+                    lastNames.get(random.nextInt(lastNames.size())),
+                    "1978-11-04"
+            ));
+        }
+    }
+
+    @Test
+    public void testUniqueID() {
+        var ids = new ArrayList<Long>();
+        authors.stream().map(Author::getID).forEach(ids::add);
+        assert ids.stream().distinct().count() == firstNames.size();
+    }
+
+    @Test
+    public void testThreadSafety() {
+        var temp = new Vector<Author>();
+        authors.parallelStream().forEach(author -> temp.add(new Author(author.getFirstName(),
+                author.getLastName(), "2010-10-07")));
+        assert temp.size() == authors.size();
+    }
+}
+
