d826ffbbb6445b70b9ba4ba1e82bb1300c20e7b5
Alexandru Ica <alexandruica703@gmail.com>
2021-03-19 17:57:53 +0200

    Similar to `AuthorTest`, test for uniqueness and thread safety

---
+++ src/test/java/Bookster/PublisherTest.java
@@ -0,0 +1,35 @@
+package Bookster;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class PublisherTest {
+    private static final ArrayList<Publisher> names = new ArrayList<>();
+
+    @BeforeAll
+    static void setup() {
+        names.add(new Publisher("Hoffman"));
+        names.add(new Publisher("Penguin"));
+        names.add(new Publisher("Macmillan"));
+        names.add(new Publisher("Collins"));
+        names.add(new Publisher("Hatchet"));
+        names.add(new Publisher("Schuster"));
+    }
+
+    @Test
+    public void testUniqueID() {
+        var ids = new ArrayList<Long>();
+        names.stream().map(Publisher::getID).forEach(ids::add);
+        assert ids.stream().distinct().count() == names.size();
+    }
+
+    @Test
+    public void testThreadSafety() {
+        var temp = new Vector<Publisher>();
+        names.parallelStream().forEach(name -> temp.add(new Publisher(name.getName())));
+        assert temp.size() == names.size();
+    }
+}
