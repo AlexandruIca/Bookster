c4e6063101e2c7a14cfb651a0b980c6f5fb1fa4f
Alexandru Ica <alexandruica703@gmail.com>
2021-03-19 20:48:41 +0200

    Test `Transaction`, similar to `Book`

---
+++ src/test/java/Bookster/TransactionTest.java
@@ -0,0 +1,30 @@
+package Bookster;
+
+import org.junit.Test;
+
+import java.util.Vector;
+import java.util.stream.IntStream;
+
+public class TransactionTest {
+    private final Author author = new Author("Some", "Author", "1990-03-13");
+    private final Publisher publisher = new Publisher("Some Publisher");
+    private final Client client = new Client("Some", "Client", "2002-02-02");
+    private final Book book = new Book("Some book", BookGenre.BIOGRAPHY, 25.99, "1998-10-02",
+            author, publisher);
+
+    @Test
+    public void testTransactionInterface() {
+        final var transact = new Transaction(this.book, this.client, "2020-07-06");
+        assert transact.getBook().getAuthors().size() == 1;
+    }
+
+    @Test
+    public void testTransactionThreadSafety() {
+        final int numIterations = 100;
+        var transactions = new Vector<Transaction>();
+        IntStream.range(0, numIterations).parallel().forEach(i -> {
+            transactions.add(new Transaction(this.book, this.client, "2019-08-07"));
+        });
+        assert transactions.size() == numIterations;
+    }
+}
