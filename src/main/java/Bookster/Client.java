3b76b529e81e4ea1ab045801192587fd068d4d12
Alexandru Ica <alexandruica703@gmail.com>
2021-03-19 18:52:28 +0200

    Implement `Client`
    Yet another class that will be used for `Book`

---
+++ src/main/java/Bookster/Client.java
@@ -0,0 +1,48 @@
+package Bookster;
+
+import java.time.LocalDate;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Client implements Unique {
+    /**
+     * Static atomic that holds increasing IDs for clients.
+     */
+    private static AtomicLong clientID = new AtomicLong(0);
+
+    /**
+     * @return a new unique ID for authors, use this when creating a new client.
+     * @apiNote Method is completely thread-safe.
+     */
+    private static Long newID() {
+        return clientID.getAndIncrement();
+    }
+
+    private final Long id;
+    private final String firstName;
+    private final String lastName;
+    private final LocalDate dateBorn;
+
+    public Client(String firstName, String lastName, String dateBorn) {
+        this.id = newID();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateBorn = LocalDate.parse(dateBorn);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getDateBorn() {
+        return dateBorn;
+    }
+
+    @Override
+    public Long getID() {
+        return this.id;
+    }
+}
