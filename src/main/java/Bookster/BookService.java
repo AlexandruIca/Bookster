f5069d62983df3cad02cd77d859e740d71411e36
Alexandru Ica <alexandruica703@gmail.com>
2021-03-20 16:51:51 +0200

    First implementation for `BookService`, currently only allows registering authors

---
+++ src/main/java/Bookster/BookService.java
@@ -0,0 +1,51 @@
+package Bookster;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+
+/**
+ * Implements all actions that can be performed in the entire app.
+ *
+ * @implNote This is a singleton implemented using an enum to assure thread safety. Also, it just
+ * seems
+ * cleaner than any other solution I've seen
+ */
+public enum BookService {
+    INSTANCE();
+
+    private HashMap<Long, Author> authors;
+
+    private BookService() {
+        authors = new HashMap<Long, Author>();
+    }
+
+    public BookService getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * @param author new author to add to the 'database'.
+     * @return true if {@code author} was added successfully, false otherwise.
+     * @apiNote false may be returned if the author is already present, or if any exception
+     * occurs when trying to add the author to {@link #authors}.
+     */
+    public boolean registerAuthor(Author author) {
+        try {
+            if (authors.containsKey(author.getID())) {
+                return false;
+            }
+            authors.put(author.getID(), author);
+        } catch (Exception e) {
+            System.out.println("[BookService::registerAuthor] Exception caught: {}" + e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    public Stream<Map.Entry<Long, Author>> authorStream() {
+        return authors.entrySet().stream();
+    }
+}
---
+++ src/test/java/Bookster/BookServiceTest.java
@@ -0,0 +1,15 @@
+package Bookster;
+
+import org.junit.jupiter.api.Test;
+
+public class BookServiceTest {
+    @Test
+    public void testRegisterAuthor() {
+        var srv = BookService.INSTANCE;
+        var auth = new Author("A", "B", "2000-01-01");
+
+        assert srv.registerAuthor(new Author("X", "Y", "1999-09-09"));
+        assert srv.registerAuthor(auth);
+        assert !srv.registerAuthor(auth);
+    }
+}
