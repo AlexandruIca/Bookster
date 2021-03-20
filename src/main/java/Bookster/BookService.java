0e164a965f628aae2dbc5b34be24a190a194b7c4
Alexandru Ica <alexandruica703@gmail.com>
2021-03-20 17:01:40 +0200

    Don't use `@note` either

--- src/main/java/Bookster/BookService.java
+++ src/main/java/Bookster/BookService.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 /**
  * Implements all actions that can be performed in the entire app.
  *
- * @note This is a singleton implemented using an enum to assure thread safety. Also, it just
+ * This is a singleton implemented using an enum to assure thread safety. Also, it just
  * seems
  * cleaner than any other solution I've seen
  */
@@ -28,7 +28,8 @@ public enum BookService {
     /**
      * @param author new author to add to the 'database'.
      * @return true if {@code author} was added successfully, false otherwise.
-     * @note false may be returned if the author is already present, or if any exception
+     *
+     * 'false' may be returned if the author is already present, or if any exception
      * occurs when trying to add the author to {@link #authors}.
      */
     public boolean registerAuthor(Author author) {
