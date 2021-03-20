91bfc7d775d2b576b1a2a01c40b67aa7961a472e
Alexandru Ica <alexandruica703@gmail.com>
2021-03-20 18:30:39 +0200

    Use the new `publisherStream` method

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -2,6 +2,7 @@ package App;
 
 import Bookster.Author;
 import Bookster.BookService;
+import Bookster.Publisher;
 
 public class App {
     private static void populate(BookService srv) {
@@ -12,11 +13,22 @@ public class App {
         srv.register(new Author("Ioan", "Slavici", "1848-01-18"));
         srv.register(new Author("Marin", "Preda", "1840-01-15"));
         srv.register(new Author("Titu", "Maiorescu", "1840-02-15"));
+
+        // publishers
+        srv.register(new Publisher("Penguin"));
+        srv.register(new Publisher("Hatchet"));
+        srv.register(new Publisher("Harper"));
+        srv.register(new Publisher("Schuster"));
+        srv.register(new Publisher("Macmillan"));
+        srv.register(new Publisher("Junimea"));
     }
 
     public static void main(String[] args) {
         BookService srv = BookService.INSTANCE;
         populate(srv);
+        System.out.println("Authors:");
         srv.authorStream().forEach(a -> System.out.println(a.getValue()));
+        System.out.println("Publishers:");
+        srv.publisherStream().forEach(p -> System.out.println(p.getValue()));
     }
 }
