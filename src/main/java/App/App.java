87e76b4b4f3e4ef8e832894f9611380357dd9992
Alexandru Ica <alexandruica703@gmail.com>
2021-03-20 20:34:54 +0200

    Use streams instead of for loops

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -1,35 +1,57 @@
 package App;
 
-import Bookster.Author;
-import Bookster.BookService;
-import Bookster.Client;
-import Bookster.Publisher;
+import Bookster.*;
+
+import java.util.Arrays;
 
 public class App {
     private static void populate(BookService srv) {
-        // authors
-        srv.register(new Author("Mihai", "Eminescu", "1850-01-15"));
-        srv.register(new Author("Ion", "Creanga", "1837-03-13"));
-        srv.register(new Author("Ion Luca", "Caragiale", "1852-03-13"));
-        srv.register(new Author("Ioan", "Slavici", "1848-01-18"));
-        srv.register(new Author("Marin", "Preda", "1840-01-15"));
-        srv.register(new Author("Titu", "Maiorescu", "1840-02-15"));
+        Author[] authors = {
+                new Author("Mihai", "Eminescu", "1850-01-15"),
+                new Author("Ion", "Creanga", "1837-03-13"),
+                new Author("Ion Luca", "Caragiale", "1852-03-13"),
+                new Author("Ioan", "Slavici", "1848-01-18"),
+                new Author("Marin", "Preda", "1840-01-15"),
+                new Author("Titu", "Maiorescu", "1840-02-15"),
+        };
 
-        // publishers
-        srv.register(new Publisher("Penguin"));
-        srv.register(new Publisher("Hatchet"));
-        srv.register(new Publisher("Harper"));
-        srv.register(new Publisher("Schuster"));
-        srv.register(new Publisher("Macmillan"));
-        srv.register(new Publisher("Junimea"));
+        Publisher[] publishers = {
+                new Publisher("Penguin"),
+                new Publisher("Hatchet"),
+                new Publisher("Harper"),
+                new Publisher("Schuster"),
+                new Publisher("Macmillan"),
+                new Publisher("Junimea"),
+        };
 
-        // clients
-        srv.register(new Client("Ema", "Ivan", "1952-04-06"));
-        srv.register(new Client("Beatrice", "Hall", "1975-08-07"));
-        srv.register(new Client("Barry", "Anderson", "1992-02-04"));
-        srv.register(new Client("Henry", "Craig", "1948-02-12"));
-        srv.register(new Client("Deann", "Alexander", "1984-01-03"));
-        srv.register(new Client("Glenda", "O'brien", "1976-10-19"));
+        Client[] clients = {
+                new Client("Ema", "Ivan", "1952-04-06"),
+                new Client("Beatrice", "Hall", "1975-08-07"),
+                new Client("Barry", "Anderson", "1992-02-04"),
+                new Client("Henry", "Craig", "1948-02-12"),
+                new Client("Deann", "Alexander", "1984-01-03"),
+                new Client("Glenda", "O'brien", "1976-10-19"),
+        };
+
+        Book[] books = {
+                new Book("Forge of the Heart", BookGenre.CRIME, 11.99, "1986-04-23", authors[0],
+                        publishers[0]),
+                new Book("The Ghost in the West", BookGenre.TRAVEL, 9.99, "2017-07-07",
+                        authors[3], publishers[2]),
+                new Book("Crimson Leviathan", BookGenre.ACTION, 39.99, "2016-11-08",
+                        authors[4], publishers[1]),
+                new Book("Male Delivery", BookGenre.BIOGRAPHY, 93.99, "2002-04-02",
+                        authors[2], publishers[3]),
+                new Book("Clue of the Crooked Puppet", BookGenre.BUSINESS, 54.99, "2009-10-13",
+                        authors[3], publishers[3]),
+        };
+
+        books[0].addAuthor(authors[1]);
+
+        Arrays.stream(authors).forEach(srv::register);
+        Arrays.stream(publishers).forEach(srv::register);
+        Arrays.stream(clients).forEach(srv::register);
+        Arrays.stream(books).forEach(srv::register);
     }
 
     public static void main(String[] args) {
@@ -41,5 +63,7 @@ public class App {
         srv.publisherStream().forEach(p -> System.out.println(p.getValue()));
         System.out.println("Clients:");
         srv.clientStream().forEach(c -> System.out.println(c.getValue()));
+        System.out.println("Books:");
+        srv.bookStream().forEach(b -> System.out.println(b.getValue()));
     }
 }
